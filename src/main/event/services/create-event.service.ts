9f611a5602c58f25aba75d37ee434c93a99581ae
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-08-04 22:05:38 +0600

    create post

--- src/main/event/services/create-event.service.ts
+++ src/main/event/services/create-event.service.ts
@@ -5,12 +5,12 @@ import { successResponse } from '@project/common/utils/response.util';
 
 @Injectable()
 export class CreateEventService {
-    constructor(private readonly prisma:PrismaService){}
-    async createEvent(dto:CreateEventDto,hostId:string){
+    constructor(private readonly prisma: PrismaService) { }
+    async createEvent(dto: CreateEventDto, hostId: string) {
         const result = await this.prisma.event.create({
             data: dto
         })
 
-        return successResponse(result,"Event Created Successfull!!")
+        return successResponse(result, "Event Created Successfull!!")
     }
 }
